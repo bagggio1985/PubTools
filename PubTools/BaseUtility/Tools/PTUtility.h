04d81d9c645dde152b72d5e6864d25bbe57df838
bagggio1985 <kyao@macdeiMac-6.local>
2016-11-21 14:22:42 +0800

    code update

--- PubTools/BaseUtility/Tools/PTUtility.h
+++ PubTools/BaseUtility/Tools/PTUtility.h
@@ -9,4 +9,6 @@
 
 @interface PTUtility : NSObject
 
++ (NSString *)currentIPAddress;
+
 @end
--- PubTools/BaseUtility/Tools/PTUtility.m
+++ PubTools/BaseUtility/Tools/PTUtility.m
@@ -5,9 +5,47 @@
 //  Created by kyao on 15/12/19.
 //
 
+#import <ifaddrs.h>
+#import <arpa/inet.h>
 #import "PTUtility.h"
 #import "PTNavigationController.h"
 
 @implementation PTUtility
+
++ (NSString *)currentIPAddress {
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    NSString *wifiAddress = nil;
+    NSString *cellAddress = nil;
+    
+    // retrieve the current interfaces - returns 0 on success
+    if(!getifaddrs(&interfaces)) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            sa_family_t sa_type = temp_addr->ifa_addr->sa_family;
+            if(sa_type == AF_INET || sa_type == AF_INET6) {
+                NSString *name = [NSString stringWithUTF8String:temp_addr->ifa_name];
+                NSString *addr = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)]; // pdp_ip0
+                //NSLog(@"NAME: \"%@\" addr: %@", name, addr); // see for yourself
+                
+                if([name isEqualToString:@"en0"])
+                    // Interface is the wifi connection on the iPhone
+                    wifiAddress = addr;
+                else
+                    if([name isEqualToString:@"pdp_ip0"])
+                        // Interface is the cell connection on the iPhone
+                        cellAddress = addr;
+                
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+        // Free memory
+        freeifaddrs(interfaces);
+    }
+    NSString *addr = wifiAddress ? wifiAddress : cellAddress;
+    return addr ? addr : @"0.0.0.0";
+}
+
 
 @end
